d3ffbb4c712e44d3a2a674a9be2af4e1e44a2b27
Noppasin Sritongkul <noppasinsritongkul@macpro.local>
2022-10-02 04:21:52 +0700

    test

---
+++ server/pingpong/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/noppas96/golang/generated"
+)
+
+// Implement pb.PingPongServer
+type PingPongServerImpl struct {
+}
+
+func (s *PingPongServerImpl) StartPing(ctx context.Context, ping *pb.Ping) (*pb.Pong, error) {
+	fmt.Println("Ping Received")
+
+	resp := pb.Pong{
+		Id:      ping.Id,
+		Message: ping.Message,
+	}
+
+	return &resp, nil
+}
+
+func StartPingPongServer() {
+	server := PingPongServerImpl{}
+
+	lis, err := net.Listen("tcp", "localhost:9000")
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterPingPongServer(grpcServer, &server)
+
+	// Start grpcServer
+	if err = grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+	fmt.Println("pingpong started")
+}
+
+func main() {
+	StartPingPongServer()
+}
